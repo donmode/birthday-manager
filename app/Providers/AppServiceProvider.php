1fa42563402d0c4c583820852c13fc495359fba8
Ernest <eosanyinlokun@socketworksng.com>
2020-04-06 21:52:28 +0100

    made modification

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use App\Validation;
 
 class AppServiceProvider extends ServiceProvider
@@ -38,6 +39,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-//        $this->registerPolicies();
+        Schema::defaultStringLength(191);
     }
 }
